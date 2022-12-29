69a4545245cb7188e25ecf36c35191faa563f562
gabriel.xavier <gabriel.landi@gmail.com>
2022-12-29 14:54:13 -0300

    Adicionando o readME, e finalizando o projeto

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let digital = document.querySelector('.digital');
+let pontSecond = document.querySelector('.p_s');
+let pontMinute = document.querySelector('.p_m');
+let pontHour = document.querySelector('.p_h');
 
 function relogio(){
     let dataAtual = new Date();
@@ -9,6 +12,10 @@ function relogio(){
     hora += ((horas<10) ? "0" : "") + horas
     hora += ((minutos<10) ? ":0" : ":") + minutos
     hora += ((segundos<10) ? ":0" : ":") + segundos
+
+    pontSecond.style.transform = `rotate(${segundos*6-90}deg)`;
+    pontMinute.style.transform = `rotate(${minutos*6-90}deg)`;
+    pontHour.style.transform = `rotate(${horas*30-90}deg)`;
 
     digital.innerHTML = hora
 }
